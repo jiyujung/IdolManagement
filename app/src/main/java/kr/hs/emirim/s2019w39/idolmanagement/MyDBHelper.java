759f20a44654ad3ed7832db1fa285b4d7f016abb
jiyujung <s2019w39@e-mirim.hs.kr>
2020-12-10 20:33:51 +0900

    Complete database

---
+++ app/src/main/java/kr/hs/emirim/s2019w39/idolmanagement/MyDBHelper.java
@@ -0,0 +1,27 @@
+package kr.hs.emirim.s2019w39.idolmanagement;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MyDBHelper extends SQLiteOpenHelper {
+    // DB 생성
+    public MyDBHelper(@Nullable Context context) {
+        super(context, "groupDB", null, 1);
+    }
+
+    // table 생성
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table groupTBL(gName CHAR(20) PRIMARY KEY, gNumber INTEGER)");
+    }
+
+    // db가 upgrade되었을 때 기존의 table을 삭제하고 새로 생성
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists groupTBL");
+        onCreate(db);
+    }
+}
